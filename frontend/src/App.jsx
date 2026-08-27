0011959c8d859a175f409b6054e503ac5a659871
Lars <aisat.robertson223391@gmail.com>
2026-08-27 18:31:41 +0800

    feat: implement user authentication and synchronization with Clerk, add axios for API requests, and integrate React Query for data fetching

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,16 @@ import ProductPage from "./pages/ProductPage";
 import ProfilePage from "./pages/ProfilePage";
 import CreatePage from "./pages/CreatePage";
 import EditProductPage from "./pages/EditProductPage";
+import useAuthReq from "./hooks/useAuthReq";
+import useUserSync from "./hooks/useUserSync";
 
 function App() {
+  const { isClerkLoaded, isSignedIn } = useAuthReq();
+  useUserSync();
+
+  console.log({ isSignedIn });
+  if (!isClerkLoaded) return null;
+
   return (
     <div className="min-h-screen bg-base-100">
       <Navbar />
